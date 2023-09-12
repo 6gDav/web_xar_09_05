749380fd6e1ef39d51965e90b9b63d97387ff6e0
6gDav <118929714+6gDav@users.noreply.github.com>
2023-09-12 09:15:23 +0200

    iawjfiawf

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const hello1 = document.querySelector('#hello')
-const container = document.querySelector('#container')
+document.addEventListener('DOMContentLoaded', function () {
+  const cardContainer = document.getElementById('cardContainer')
+  const addCardButton = document.getElementById('addCardButton')
 
-hello1.addEventListener('click', () => {
-  let card = document.createElement('div')
-  card.classList.add('card')
-  card.innerText = Math.floor(Math.random() * 6 + 1)
-  container.append(card)
+  function createRandomCard() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.textContent = randomNumber
+    cardContainer.appendChild(card)
+  }
+
+  addCardButton.addEventListener('click', createRandomCard)
+
+  // Create 10 initial random cards
+  for (let i = 0; i < 10; i++) {
+    createRandomCard()
+  }
 })
-/*
-function generateRandomNumber() {
-  const idkDiv = document.querySelector('#idk')
-  const randomNumber = Math.floor(Math.random() * 100) + 1
-  idkDiv.innerHTML = '<p>Véletlenszerű szám: ' + randomNumber + '</p>'
-}
-*/
